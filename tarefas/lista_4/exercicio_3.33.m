665d30860855203d1a10c992eca8623c385c744d
JGlims <jg.melo.lima2005@gmail.com>
2026-06-15 09:57:02 -0300

    mud

--- tarefas/lista_4/exercicio_3.33.m
+++ tarefas/lista_4/exercicio_3.33.m
@@ -1,13 +1,12 @@
-% Exercicio 3.33 - Versao Blindada (Octave)
-pkg load signal % Forcamos o load do pacote por precaucao
+% Exercicio 3.33 - Correcao do Overlap para o Octave
+pkg load signal 
 
 load('eeg_data.mat'); % Carrega a variavel 'eeg'
 
 fs = 50;
 N = length(eeg);
 
-% FORCA a ser vetor coluna. Incompatibilidade de dimensoes 
-% e a maior causadora de falhas no pwelch do Octave.
+% Forca para vetor coluna e remove o DC
 eeg_col = eeg(:); 
 eeg_centered = eeg_col - mean(eeg_col);
 
@@ -16,10 +15,12 @@ Pxx_no_avg = (abs(fft(eeg_centered)).^2) / N;
 f_no_avg = (0:N-1)' * (fs / N);
 
 % 2. Espectro COM calculo de media (Metodo de Welch)
-L = 256; 
-% Passar 'L' faz o Octave gerar a janela Hamming do tamanho certo sozinho.
-% Passar '0.99' indica a fracao de sobreposicao.
-[Pxx_welch, f_welch] = pwelch(eeg_centered, L, 0.99, L, fs);
+L = 256; % Tamanho do segmento
+% Calculamos o numero exato e inteiro de amostras para dar os 99%
+noverlap = floor(0.99 * L); 
+
+% Passamos a janela (hamming) e o numero de amostras calculadas
+[Pxx_welch, f_welch] = pwelch(eeg_centered, hamming(L), noverlap, L, fs);
 
 % Plotagem Comparativa
 figure;
@@ -29,7 +30,7 @@ plot(f_no_avg(1:floor(N/2)), Pxx_no_avg(1:floor(N/2)), 'b');
 title('Espectro de Potencia (Sem Media - Periodograma)');
 xlabel('Frequencia (Hz)'); 
 ylabel('Potencia');
-xlim([0 25]); % Forca a visualizacao apenas ate Nyquist (fs/2)
+xlim([0 25]); % Frequencia de Nyquist
 
 subplot(2,1,2);
 plot(f_welch, Pxx_welch, 'r', 'LineWidth', 1.5);
