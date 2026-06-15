f2c7d4812c0a549083966b59b606e3b4d22fdaef
JGlims <jg.melo.lima2005@gmail.com>
2026-06-15 10:02:04 -0300

    d

--- tarefas/lista_4/exercicio_3.33.m
+++ tarefas/lista_4/exercicio_3.33.m
@@ -1,4 +1,4 @@
-% Exercicio 3.33 - Correcao do Overlap para o Octave
+% Exercicio 3.33 - Adaptado para limitacoes do Octave
 pkg load signal 
 
 load('eeg_data.mat'); % Carrega a variavel 'eeg'
@@ -16,10 +16,10 @@ f_no_avg = (0:N-1)' * (fs / N);
 
 % 2. Espectro COM calculo de media (Metodo de Welch)
 L = 256; % Tamanho do segmento
-% Calculamos o numero exato e inteiro de amostras para dar os 99%
-noverlap = floor(0.99 * L); 
+% LIMITACAO DO OCTAVE: O pacote signal trava com overlaps > 0.95.
+% Usaremos 0.95 (95%) no lugar dos 0.99 (99%) pedidos na questao.
+noverlap = 0.95; 
 
-% Passamos a janela (hamming) e o numero de amostras calculadas
 [Pxx_welch, f_welch] = pwelch(eeg_centered, hamming(L), noverlap, L, fs);
 
 % Plotagem Comparativa
@@ -34,7 +34,7 @@ xlim([0 25]); % Frequencia de Nyquist
 
 subplot(2,1,2);
 plot(f_welch, Pxx_welch, 'r', 'LineWidth', 1.5);
-title(['Espectro de Potencia (Metodo de Welch) - Segmento: ' num2str(L)]);
+title(['Espectro de Potencia (Welch) - Segmento: ' num2str(L) ' (95% overlap)']);
 xlabel('Frequencia (Hz)'); 
 ylabel('Potencia');
 xlim([0 25]);
